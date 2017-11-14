04e605f666fc787d9d7285ff5ab873cc92548d6b
Carlos <thehandyowl.io@gmail.com>
2017-11-15 00:19:01 +0100

    Activando ActionBar, mostrando título, y navegación con botón para volver a la vista principal

--- app/src/main/java/com/tho/nigirimoe/activity/TableActivity.kt
+++ app/src/main/java/com/tho/nigirimoe/activity/TableActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.MenuItem
 import com.tho.nigirimoe.R
+import com.tho.nigirimoe.model.Table
+import com.tho.nigirimoe.model.Tables
 
 import kotlinx.android.synthetic.main.activity_table.*
 
@@ -23,17 +26,38 @@ class TableActivity : AppCompatActivity() {
         }
     }
 
+    private val tableIndex: Int by lazy { intent.getIntExtra(EXTRA_TABLE_ITEM, 0) }
+    private val table: Table by lazy { Tables[intent.getIntExtra(EXTRA_TABLE_ITEM, 0)] }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table)
-        setSupportActionBar(toolbar)
 
-        Log.v("TAG", "Pasando item: ${intent.getIntExtra(EXTRA_TABLE_ITEM, 0)}")
+        Log.v("TAG", "Pasando item: ${tableIndex} y mesa: ${table.toString()}")
 
         new_order_btn.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        setupActionBar()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = table.name
     }
 
 }
