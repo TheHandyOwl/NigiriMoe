1b6ff8f9664db13ea56c801e19fc1103308fdcde
Carlos <thehandyowl.io@gmail.com>
2017-11-19 23:19:03 +0100

    FakeData: Mostramos los pedidos de cada mesa y detectamos pulsación sobre las celdas

--- app/src/main/java/com/tho/nigirimoe/activity/TableActivity.kt
+++ app/src/main/java/com/tho/nigirimoe/activity/TableActivity.kt
@@ -7,9 +7,10 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.tho.nigirimoe.R
-import com.tho.nigirimoe.model.Table
-import com.tho.nigirimoe.model.Tables
+import com.tho.nigirimoe.model.*
 
 import kotlinx.android.synthetic.main.activity_table.*
 
@@ -35,12 +36,11 @@ class TableActivity : AppCompatActivity() {
 
         Log.v("TAG", "Pasando item: ${tableIndex} y mesa: ${table.toString()}")
 
-        new_order_btn.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        //addFakeData()
 
         setupActionBar()
+        setupOrdersList()
+        setupNewOrderButton()
 
     }
 
@@ -54,10 +54,35 @@ class TableActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    fun addFakeData() {
+        if(MenuList.courses.size > 0) {
+            for (item in 1..tableIndex+1) {
+                table.orders.add(Order(MenuList.courses[0], "Sin observaciones"))
+            }
+        }
+    }
+
     fun setupActionBar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = table.name
+    }
+
+    private fun setupOrdersList() {
+        val list = findViewById<ListView>(R.id.orders_list)
+        val adapter = ArrayAdapter<Order>(this,android.R.layout.simple_list_item_1, table.orders)
+        list.adapter = adapter
+
+        list.setOnItemClickListener { _, _, position, _ ->
+            Log.v("TAG", "Se ha pulsado en la celda: ${position}")
+        }
+    }
+
+    private fun setupNewOrderButton() {
+        new_order_btn.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
     }
 
 }
